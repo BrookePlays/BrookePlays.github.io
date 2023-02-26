baa48ecb9ed0af56ae5c93c067a1456c0a64eedb
BrookePlays <81758942+BrookePlays@users.noreply.github.com>
2023-02-26 12:35:26 -0600

    Update load.js
    can beautifying solve my dumb code

--- js/load.js
+++ js/load.js
@@ -504,379 +504,379 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 	inputs.onchange = function() {
 		localStorage['selection'] = this.value;
-	}
+	};
 	fill00.onchange = function() {
 		localStorage['filler00'] = this.value;
-	}
+	};
 	fill01.onchange = function() {
 		localStorage['filler01'] = this.value;
-	}
+	};
 	fill02.onchange = function() {
 		localStorage['filler02'] = this.value;
-	}
+	};
 	fill03.onchange = function() {
 		localStorage['filler03'] = this.value;
-	}
+	};
 	fill04.onchange = function() {
 		localStorage['filler04'] = this.value;
-	}
+	};
 	fill10.onchange = function() {
 		localStorage['filler10'] = this.value;
-	}
+	};
 	fill11.onchange = function() {
 		localStorage['filler11'] = this.value;
-	}
+	};
 	fill12.onchange = function() {
 		localStorage['filler12'] = this.value;
-	}
+	};
 	fill13.onchange = function() {
 		localStorage['filler13'] = this.value;
-	}
+	};
 	fill14.onchange = function() {
 		localStorage['filler14'] = this.value;
-	}
+	};
 	fill20.onchange = function() {
 		localStorage['filler20'] = this.value;
-	}
+	};
 	fill21.onchange = function() {
 		localStorage['filler21'] = this.value;
-	}
+	};
 	fill22.onchange = function() {
 		localStorage['filler22'] = this.value;
-	}
+	};
 	fill23.onchange = function() {
 		localStorage['filler23'] = this.value;
-	}
+	};
 	fill24.onchange = function() {
 		localStorage['filler24'] = this.value;
-	}
+	};
 	fill30.onchange = function() {
 		localStorage['filler30'] = this.value;
-	}
+	};
 	fill31.onchange = function() {
 		localStorage['filler31'] = this.value;
-	}
+	};
 	fill32.onchange = function() {
 		localStorage['filler32'] = this.value;
-	}
+	};
 	fill33.onchange = function() {
 		localStorage['filler33'] = this.value;
-	}
+	};
 	fill34.onchange = function() {
 		localStorage['filler34'] = this.value;
-	}
+	};
 	input.onchange = function() {
 		localStorage['class'] = this.value;
-	}
+	};
 	input0.onchange = function() {
 		localStorage['class0'] = this.value;
-	}
+	};
 	input09.onchange = function() {
 		localStorage['class09'] = this.value;
-	}
+	};
 	input08.onchange = function() {
 		localStorage['class08'] = this.value;
-	}
+	};
 	input07.onchange = function() {
 		localStorage['class07'] = this.value;
-	}
+	};
 	input06.onchange = function() {
 		localStorage['class06'] = this.value;
-	}
+	};
 	input05.onchange = function() {
 		localStorage['class05'] = this.value;
-	}
+	};
 	input04.onchange = function() {
 		localStorage['class04'] = this.value;
-	}
+	};
 	input03.onchange = function() {
 		localStorage['class03'] = this.value;
-	}
+	};
 	input02.onchange = function() {
 		localStorage['class02'] = this.value;
-	}
+	};
 	input01.onchange = function() {
 		localStorage['class01'] = this.value;
-	}
+	};
 	input00.onchange = function() {
 		localStorage['class00'] = this.value;
-	}
+	};
 	input1.onchange = function() {
 		localStorage['class1'] = this.value;
-	}
+	};
 	input10.onchange = function() {
 		localStorage['class10'] = this.value;
-	}
+	};
 	input11.onchange = function() {
 		localStorage['class11'] = this.value;
-	}
+	};
 	input12.onchange = function() {
 		localStorage['class12'] = this.value;
-	}
+	};
 	input13.onchange = function() {
 		localStorage['class13'] = this.value;
-	}
+	};
 	input14.onchange = function() {
 		localStorage['class14'] = this.value;
-	}
+	};
 	input15.onchange = function() {
 		localStorage['class15'] = this.value;
-	}
+	};
 	input16.onchange = function() {
 		localStorage['class16'] = this.value;
-	}
+	};
 	input2.onchange = function() {
 		localStorage['class2'] = this.value;
-	}
+	};
 	input20.onchange = function() {
 		localStorage['class20'] = this.value;
-	}
+	};
 	input21.onchange = function() {
 		localStorage['class21'] = this.value;
-	}
+	};
 	input22.onchange = function() {
 		localStorage['class22'] = this.value;
-	}
+	};
 	input23.onchange = function() {
 		localStorage['class23'] = this.value;
-	}
+	};
 	input24.onchange = function() {
 		localStorage['class24'] = this.value;
-	}
+	};
 	input25.onchange = function() {
 		localStorage['class25'] = this.value;
-	}
+	};
 	input26.onchange = function() {
 		localStorage['class26'] = this.value;
-	}
+	};
 	input27.onchange = function() {
 		localStorage['class27'] = this.value;
-	}
+	};
 	input28.onchange = function() {
 		localStorage['class28'] = this.value;
-	}
+	};
 	input29.onchange = function() {
 		localStorage['class29'] = this.value;
-	}
+	};
 	input2a.onchange = function() {
 		localStorage['class2a'] = this.value;
-	}
+	};
 	input2b.onchange = function() {
 		localStorage['class2b'] = this.value;
-	}
+	};
 	input2c.onchange = function() {
 		localStorage['class2c'] = this.value;
-	}
+	};
 	input2d.onchange = function() {
 		localStorage['class2d'] = this.value;
-	}
+	};
 	input2e.onchange = function() {
 		localStorage['class2e'] = this.value;
-	}
+	};
 	input2f.onchange = function() {
 		localStorage['class2f'] = this.value;
-	}
+	};
 	input2g.onchange = function() {
 		localStorage['class2g'] = this.value;
-	}
+	};
 	input2h.onchange = function() {
 		localStorage['class2h'] = this.value;
-	}
+	};
 	input2i.onchange = function() {
 		localStorage['class2i'] = this.value;
-	}
+	};
 	input2j.onchange = function() {
 		localStorage['class2j'] = this.value;
-	}
+	};
 	input2k.onchange = function() {
 		localStorage['class2k'] = this.value;
-	}
+	};
 	input2l.onchange = function() {
 		localStorage['class2l'] = this.value;
-	}
+	};
 	input2m.onchange = function() {
 		localStorage['class2m'] = this.value;
-	}
+	};
 	input3.onchange = function() {
 		localStorage['class3'] = this.value;
-	}
+	};
 	input30.onchange = function() {
 		localStorage['class30'] = this.value;
-	}
+	};
 	input31.onchange = function() {
 		localStorage['class31'] = this.value;
-	}
+	};
 	input32.onchange = function() {
 		localStorage['class32'] = this.value;
-	}
+	};
 	input33.onchange = function() {
 		localStorage['class33'] = this.value;
-	}
+	};
 	input34.onchange = function() {
 		localStorage['class34'] = this.value;
-	}
+	};
 	input35.onchange = function() {
 		localStorage['class35'] = this.value;
-	}
+	};
 	input36.onchange = function() {
 		localStorage['class36'] = this.value;
-	}
+	};
 	input4.onchange = function() {
 		localStorage['class4'] = this.value;
-	}
+	};
 	input40.onchange = function() {
 		localStorage['class40'] = this.value;
-	}
+	};
 	input41.onchange = function() {
 		localStorage['class41'] = this.value;
-	}
+	};
 	input42.onchange = function() {
 		localStorage['class42'] = this.value;
-	}
+	};
 	input5.onchange = function() {
 		localStorage['class5'] = this.value;
-	}
+	};
 	input50.onchange = function() {
 		localStorage['class50'] = this.value;
-	}
+	};
 	input51.onchange = function() {
 		localStorage['class51'] = this.value;
-	}
+	};
 	input52.onchange = function() {
 		localStorage['class52'] = this.value;
-	}
+	};
 	input53.onchange = function() {
 		localStorage['class53'] = this.value;
-	}
+	};
 	input54.onchange = function() {
 		localStorage['class54'] = this.value;
-	}
+	};
 	input55.onchange = function() {
 		localStorage['class55'] = this.value;
-	}
+	};
 	input56.onchange = function() {
 		localStorage['class56'] = this.value;
-	}
+	};
 	input57.onchange = function() {
 		localStorage['class57'] = this.value;
-	}
+	};
 	input58.onchange = function() {
 		localStorage['class58'] = this.value;
-	}
+	};
 	input59.onchange = function() {
 		localStorage['class59'] = this.value;
-	}
+	};
 	input5a.onchange = function() {
 		localStorage['class5a'] = this.value;
-	}
+	};
 	input5b.onchange = function() {
 		localStorage['class5b'] = this.value;
-	}
+	};
 	input5c.onchange = function() {
 		localStorage['class5c'] = this.value;
-	}
+	};
 	input5d.onchange = function() {
 		localStorage['class5d'] = this.value;
-	}
+	};
 	input5e.onchange = function() {
 		localStorage['class5e'] = this.value;
-	}
+	};
 	input6.onchange = function() {
 		localStorage['class6'] = this.value;
-	}
+	};
 	input65.onchange = function() {
 		localStorage['class65'] = this.value;
-	}
+	};
 	input60.onchange = function() {
 		localStorage['class60'] = this.value;
-	}
+	};
 	input66.onchange = function() {
 		localStorage['class66'] = this.value;
-	}
+	};
 	input61.onchange = function() {
 		localStorage['class61'] = this.value;
-	}
+	};
 	input62.onchange = function() {
 		localStorage['class62'] = this.value;
-	}
+	};
 	input63.onchange = function() {
 		localStorage['class63'] = this.value;
-	}
+	};
 	input64.onchange = function() {
 		localStorage['class64'] = this.value;
-	}
+	};
 	input7.onchange = function() {
 		localStorage['class7'] = this.value;
-	}
+	};
 	input75.onchange = function() {
 		localStorage['class75'] = this.value;
-	}
+	};
 	input70.onchange = function() {
 		localStorage['class70'] = this.value;
-	}
+	};
 	input76.onchange = function() {
 		localStorage['class76'] = this.value;
-	}
+	};
 	input71.onchange = function() {
 		localStorage['class71'] = this.value;
-	}
+	};
 	input72.onchange = function() {
 		localStorage['class72'] = this.value;
-	}
+	};
 	input73.onchange = function() {
 		localStorage['class73'] = this.value;
-	}
+	};
 	input74.onchange = function() {
 		localStorage['class74'] = this.value;
-	}
+	};
 	input8.onchange = function() {
 		localStorage['class8'] = this.value;
-	}
+	};
 	input85.onchange = function() {
 		localStorage['class85'] = this.value;
-	}
+	};
 	input80.onchange = function() {
 		localStorage['class80'] = this.value;
-	}
+	};
 	input86.onchange = function() {
 		localStorage['class86'] = this.value;
-	}
+	};
 	input81.onchange = function() {
 		localStorage['class81'] = this.value;
-	}
+	};
 	input82.onchange = function() {
 		localStorage['class82'] = this.value;
-	}
+	};
 	input83.onchange = function() {
 		localStorage['class83'] = this.value;
-	}
+	};
 	input84.onchange = function() {
 		localStorage['class84'] = this.value;
-	}
+	};
 	input9.onchange = function() {
 		localStorage['class9'] = this.value;
-	}
+	};
 	input95.onchange = function() {
 		localStorage['class95'] = this.value;
-	}
+	};
 	input90.onchange = function() {
 		localStorage['class90'] = this.value;
-	}
+	};
 	input96.onchange = function() {
 		localStorage['class96'] = this.value;
-	}
+	};
 	input91.onchange = function() {
 		localStorage['class91'] = this.value;
-	}
+	};
 	input92.onchange = function() {
 		localStorage['class92'] = this.value;
-	}
+	};
 	input93.onchange = function() {
 		localStorage['class93'] = this.value;
-	}
+	};
 	input94.onchange = function() {
 		localStorage['class94'] = this.value;
-	}
+	};
 	document.getElementById("rec").defaultChecked;
-	calc()
+	calc();
 });
