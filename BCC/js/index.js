9a07c4feacc1cf853af9a7be3bd8ce42bf50a5d5
BrookePlays <81758942+BrookePlays@users.noreply.github.com>
2023-11-17 01:14:38 -0600

    Delete BCC directory

--- BCC/js/index.js
+++
@@ -1,9 +0,0 @@
-
-    var checked = JSON.parse(localStorage.getItem('msclass')), classms=0,
-    checked0 = JSON.parse(localStorage.getItem('msclass0')), classms0=0,
-    checked1 = JSON.parse(localStorage.getItem('msclass1')), classms1=0;
-    
-    function deleteItems() {
-      localStorage.clear();
-      location.reload();
-    }
